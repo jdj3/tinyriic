a68682102b65416e4d63dbb3ed3222527d8a0bbe
John Jackman <jdjackman@gmail.com>
2016-08-27 12:39:01 -0400

    i386 build

---
+++ src/i386/arch_unistd.h
@@ -0,0 +1,11 @@
+#ifndef ARCH_UNISTD_H
+#define ARCH_UNISTD_H
+
+#define NR_exit     (1)
+#define NR_read     (3)
+#define NR_write    (4)
+#define NR_ptrace   (26)
+#define NR_wait4    (114)
+#define NR_mmap     (192)
+
+#endif
